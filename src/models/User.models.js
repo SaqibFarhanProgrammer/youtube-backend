66ceb63c23e9143a64954828c6047e10b6b6f58e
shahzaib887 <shahzaibshazzo@gmail.com>
2025-12-17 17:53:48 +0500

    user model created

--- src/models/User.models.js
+++ src/models/User.models.js
@@ -1,11 +1,55 @@
 import mongoose from 'mongoose';
 
-const userschema = new mongoose.Schema({
-  id: {
-    type: String,
+const userschema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    Fullname: {
+      type: String,
+      required: true,
+      unique: true,
+
+      trim: true,
+      index: true,
+    },
+    image: {
+      type: String, //cloudnery
+      required: true,
+    },
+
+    coverImage: {
+      type: String,
+    },
+    watchHistory: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'video',
+      },
+    ],
+    password: {
+      type: String,
+      required: [true, 'password is required'],
+    },
+    refreshTokens: {
+      type: String,
+    },
   },
-  WatchHistory: {
-    type: Array,
-    default: [],
-  },
-});
+  {
+    timestamps: true,
+  }
+);
+
+export const User = mongoose.model('User', userschema);
