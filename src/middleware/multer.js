fb48f0e319b16a92f6dc7233d645fb833092613f
shahzaib887 <shahzaibshazzo@gmail.com>
2025-12-18 18:10:31 +0500

    a

--- src/middleware/multer.js
+++ src/middleware/multer.js
@@ -1,1 +1,12 @@
-import  multer  from "multer";
+import multer from 'multer';
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, '/tmp/my-uploads');
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+    cb(null, file.fieldname + '-' + uniqueSuffix);
+  },
+});
+
+console.log(Date.now() + '-' + Math.round(Math.random() * 1e9));
