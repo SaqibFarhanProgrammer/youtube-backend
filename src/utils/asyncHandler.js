efce0b2acc039cfcaea0f03c445c3e4af4086dfb
shahzaib887 <shahzaibshazzo@gmail.com>
2025-12-19 21:20:29 +0500

    all sett !!!

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,18 +1,7 @@
-const asynchandler = () => {
-  (req, res, next) => {
+const asyncHandler = (fn) => {
+  return (req, res, next) => {
     Promise.resolve(fn(req, res, next)).catch((err) => next(err));
   };
 };
 
-// const asynchandler = (fn) => async (req, res, next) => {
-//     try {
-
-//     } catch (error) {
-//         res.status(500).json({
-//             success: false,
-//             message: error.message,
-//         });
-//     }
-// }
-
-export { asynchandler };
+export default asyncHandler;
