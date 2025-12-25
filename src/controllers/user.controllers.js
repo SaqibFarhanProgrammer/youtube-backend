97440908d79a51c901c3c3b7d0f2f091378b068c
shahzaib887 <shahzaibshazzo@gmail.com>
2025-12-25 15:27:41 +0500

    register user

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,9 +1,27 @@
 import asyncHandler from '../utils/asyncHandler.js';
 
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: 'hillo gggg',
-  });
+  /*
+// Step 1: Extract user data from request body
+
+// Step 2: Validate that required fields are present
+
+
+// Step 3: Hash the password
+// Step 4: Save user to database
+
+// Step 5: Return success response
+
+
+
+
+*/
+
+const {email , password} = req.body;
+console.log(email,password);
+
+
+
 });
 
 export { registerUser };
